e6a8685d75b4dc03e49cbe024bba3e0feac7fd34
jlagneau <jlagneau@student.42.fr>
2017-03-18 10:57:20 +0100

    Disable the ability to see the link to like pictures if not connected

--- src/views/show.php
+++ src/views/show.php
@@ -1,19 +1,19 @@
 <main id="show-picture">
     <img src="<?php echo $picture->getPath() ?>" alt="" />
     <p><?php echo $likes ?> Likes</p>
-    <a href="/like?id=<?php echo $picture->getId() ?>">
-    <?php if ($hasLiked) : ?>
-        You already liked this picture!
-    <?php else : ?>
-        Do you like it?
-    <?php endif ?>
-     </a>
     <?php if ($connected) : ?>
-    <h2>Post a comment: </h2>
-    <form id="comment-form" action="/comment?id=<?php echo $picture->getId() ?>" method="POST">
-        <textarea name="content"></textarea><br />
-        <input type="submit" value="Post comment" />
-    </form>
+        <a href="/like?id=<?php echo $picture->getId() ?>">
+        <?php if ($hasLiked) : ?>
+            You already liked this picture!
+        <?php else : ?>
+            Do you like it?
+        <?php endif ?>
+        </a>
+        <h2>Post a comment: </h2>
+        <form id="comment-form" action="/comment?id=<?php echo $picture->getId() ?>" method="POST">
+            <textarea name="content"></textarea><br />
+            <input type="submit" value="Post comment" />
+        </form>
     <?php endif ?>
     <h2>Comments: </h2>
     <ul id="comments-section">
