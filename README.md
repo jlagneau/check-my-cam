bafc7889562f265659f1939759c79901d0017367
jlagneau <jlagneau@student.42.fr>
2017-03-19 06:23:20 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# Check my cam
+
+Web project without any type of external library / Framework allowed (Vanilla PHP, JS, CSS).
+
+## Tests
+
+To run the website, just run this command from the project root directory:
+
+```
+php -S 127.0.0.1:8001 router.php
+```
--- router.php
+++ router.php
@@ -7,7 +7,7 @@
 //   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2017/03/19 05:51:13 by jlagneau          #+#    #+#             //
-//   Updated: 2017/03/19 05:51:21 by jlagneau         ###   ########.fr       //
+//   Updated: 2017/03/19 06:19:22 by jlagneau         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -18,7 +18,7 @@ if (php_sapi_name() == 'cli-server') {
     if ('/config/setup.php' === $_SERVER['REQUEST_URI'] &&
         in_array($_SERVER['REMOTE_ADDR'], ['::1', '127.0.0.1'])) {
         return false;
-    } elseif (preg_match('#^/(\.|src|config|router.php)#', $_SERVER['REQUEST_URI'])) {
+    } elseif (preg_match('#^/(\.|src|config|router.php|README.md)#', $_SERVER['REQUEST_URI'])) {
         header('Location: /404');
     } else {
         return false;
